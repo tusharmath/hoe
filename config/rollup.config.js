4b6dd7ed885020c76e17b46f7d909b52186efcbc
Tushar Mathur <tusharmath@gmail.com>
2017-04-26 00:31:38 +0530

    refactor(rollup): publish a umd version

---
+++ config/rollup.config.js
@@ -0,0 +1,21 @@
+/**
+ * Created by tushar on 26/04/17.
+ */
+
+'use strict'
+
+
+import commonjs from "rollup-plugin-commonjs";
+import minify from "rollup-plugin-babili";
+
+export default {
+  exports: 'named',
+  entry: './hoe.js',
+  dest: './dist/hoe.js',
+  format: 'umd',
+  moduleName: 'Hoe',
+  plugins: [
+    minify({comments: false}),
+    commonjs({}),
+  ]
+}
