be61a78266602696f0330f8f3f5ae5efee75cebe
Tushar Mathur <tusharmath@gmail.com>
2018-07-01 11:49:08 +0530

    fix(package): update dependencies and migrate to mocha

--- test/test.hoe.ts
+++ test/test.hoe.ts
@@ -1,7 +1,8 @@
 /**
  * Created by tushar on 15/01/17.
  */
-import test from 'ava'
+
+import * as assert from 'assert'
 import {action} from 'action-type'
 import {create} from '../hoe'
 
@@ -13,51 +14,50 @@ export const testListener = () => {
   return {actions, listener}
 }
 
-test(t => {
-  const {actions, listener} = testListener()
-  const e = create(listener)
-  e.emit(100)
-  e.emit(200)
-  t.deepEqual(actions, [100, 200])
-})
+describe('hoe', () => {
+  it('should emit actions', () => {
+    const {actions, listener} = testListener()
+    const e = create(listener)
+    e.emit(100)
+    e.emit(200)
+    assert.deepEqual(actions, [100, 200])
+  })
+  it('should emit nested action with type', () => {
+    const {actions, listener} = testListener()
+    const e = create(listener).of('T')
+    e.emit(100)
+    e.emit(200)
+    assert.deepEqual(actions, [action('T', 100), action('T', 200)])
+  })
+  it('should nested actions', () => {
+    const {actions, listener} = testListener()
+    const e = create(listener)
+      .of('A')
+      .of('B')
+    e.emit(100)
+    e.emit(200)
+    assert.deepEqual(actions, [
+      action('A', action('B', 100)),
+      action('A', action('B', 200))
+    ])
+  })
 
-test('of()', t => {
-  const {actions, listener} = testListener()
-  const e = create(listener).of('T')
-  e.emit(100)
-  e.emit(200)
-  t.deepEqual(actions, [action('T', 100), action('T', 200)])
-})
-
-test('of(A).of(B)', t => {
-  const {actions, listener} = testListener()
-  const e = create(listener)
-    .of('A')
-    .of('B')
-  e.emit(100)
-  e.emit(200)
-  t.deepEqual(actions, [
-    action('A', action('B', 100)),
-    action('A', action('B', 200))
-  ])
-})
-
-test('emit.bind()', t => {
-  const {actions, listener} = testListener()
-  const e = create(listener)
-  const f = e.of('F')
-  e.emit.call(null, 100)
-  f.emit.call(null, 200)
-  t.deepEqual(actions, [100, action('F', 200)])
-})
-
-test('stack safety', t => {
-  const {listener} = testListener()
-  let e = create(listener)
-  t.notThrows(() => {
-    for (var i = 0; i < 1e6; i++) {
-      e = e.of(i.toString())
-    }
-    e.emit(null)
+  it('should persist arguments', () => {
+    const {actions, listener} = testListener()
+    const e = create(listener)
+    const f = e.of('F')
+    e.emit.call(null, 100)
+    f.emit.call(null, 200)
+    assert.deepEqual(actions, [100, action('F', 200)])
+  })
+  it('should be stack safe', () => {
+    const {listener} = testListener()
+    let e = create(listener)
+    assert.doesNotThrow(() => {
+      for (var i = 0; i < 1e6; i++) {
+        e = e.of(i.toString())
+      }
+      e.emit(null)
+    })
   })
 })
